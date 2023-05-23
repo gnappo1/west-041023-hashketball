7729b7b387e0af0e06d3e0f34ab00e73ac02cb6b
Matteo Piccini <gnappo.1@live.it>
2023-05-23 16:13:08 -0700

    post walkthrough code

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -1,3 +1,6 @@
+# import ipdb
+from functools import reduce
+
 def game_dict():
     return {
         "home": {
@@ -182,3 +185,78 @@ def game_dict():
             ]
         }
     }
+
+#! Helpers
+def all_players_in_dict():
+    all_players_dict = {}
+    for team in ['home', 'away']:
+        for player in game_dict()[team]['players']:
+            all_players_dict[player.get('name')] = player
+    return all_players_dict
+# print(all_players_in_dict())
+
+def all_players_in_list():
+    return game_dict().get('home').get('players') + game_dict().get('away').get('players')
+# print(all_players_in_list())
+
+#! Deliverables
+def num_points_per_game(player_name):
+    game_obj = game_dict()
+    for key in game_obj.keys():
+        team_players = game_obj.get(key).get('players')
+        # filtered_players_scores = [player.get('points_per_game') for player in team_players if player_name == player.get('name')]
+        # if len(filtered_players_scores) > 0:
+        if filtered_players_scores := [
+            player.get('points_per_game')
+            for player in team_players
+            if player_name == player.get('name')
+        ]:
+            return filtered_players_scores[0]
+    return "Player is not in any teams!"
+
+# print(num_points_per_game("Davis Bertanssss"))
+
+def player_age_old(player_name):
+    game_obj = game_dict()
+    for key in game_obj.keys():
+        team_players = game_obj.get(key).get('players')
+        # filtered_players_scores = [player.get('points_per_game') for player in team_players if player_name == player.get('name')]
+        # if len(filtered_players_scores) > 0:
+        if filtered_players_scores := [
+            player.get('age')
+            for player in team_players
+            if player_name == player.get('name')
+        ]:
+            return filtered_players_scores[0]
+    return "Player is not in any teams!"
+
+def player_age(player_name):
+    return all_players_in_dict().get(player_name).get('age') if all_players_in_dict().get(player_name) else "Player's not in here!!!"
+    # research options for the safe navigation operator!
+    
+    # if player_name in all_players_in_dict().keys():
+    #     return all_players_in_dict().get(player_name).get('age')
+    # return "Player's not in our teams!"
+
+print(player_age("Davis Bertans"))
+
+def logic_for_reduce(sum, el):
+    return sum + el
+
+def average_rebounds_by_shoe_brand():
+    total_rebounds_by_brand = {}
+    for player in all_players_in_list():
+        current_player_brand = player.get('shoe_brand')
+        current_player_rebounds = player.get('rebounds_per_game')
+        
+        #line under: check if the brand is already a key inside the final dict
+        if current_player_brand in total_rebounds_by_brand:
+            total_rebounds_by_brand.get(current_player_brand).append(current_player_rebounds)
+        else:
+            total_rebounds_by_brand[current_player_brand] = [current_player_rebounds]
+    for k, v in total_rebounds_by_brand.items():
+        avg = reduce(lambda sum, el: sum + el, v) / len(v)
+        # print(f"{k}: {format(avg, '.2f')}")
+        print(f"{k}: {avg:.2f}")
+        
+average_rebounds_by_shoe_brand()
--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from basket_ball import (
-    num_points_per_game, player_age,
-    team_colors, team_names,
-    player_numbers, player_stats,
-    average_rebounds_by_shoe_brand
+    num_points_per_game#, player_age,
+    #team_colors, team_names,
+    #player_numbers, player_stats,
+    #average_rebounds_by_shoe_brand
 )
 
 if __name__ == '__main__':
